bc9be770a894a96e2979569cf8115e5841b4ec02
daniel <Danielpachecopz@gmail.com>
2024-09-29 06:58:46 -0500

    se crearon las propiedades y la lista de menuitmens

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MenuItems {
+  final String title;
+  final String subtittle;
+  final String url;
+  final IconData icon;
+
+  const MenuItems(
+      {required this.title,
+      required this.subtittle,
+      required this.url,
+      required this.icon});
+}
+
+const List<MenuItems> menuItemsList = [
+  MenuItems(
+      title: 'Buttons',
+      subtittle: 'first widget',
+      url: '/buttons',
+      icon: Icons.accessibility_new_sharp),
+  MenuItems(
+      title: 'Cards ',
+      subtittle: 'card widgets',
+      url: '/cards',
+      icon: Icons.card_membership_outlined)
+];
